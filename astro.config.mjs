6bbff8da5482518524ca85b26e9e2a3fa1eae95b
Davi-ldc <96698943+Davi-ldc@users.noreply.github.com>
2025-10-30 20:45:14 -0300

    chore: update package.json to include bun as package manager and add deployment scripts on cloudflare for better loading time

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,10 @@
-import { defineConfig } from "astro/config";
-
 import tailwindcss from "@tailwindcss/vite";
-
-import vercel from "@astrojs/vercel";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "static",
   vite: {
     plugins: [tailwindcss()],
   },
-
-  adapter: vercel(),
 });
